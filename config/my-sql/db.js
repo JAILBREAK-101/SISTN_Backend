2102b8d09f2423a52b7c93c85ec2ba55e608438b
Oluwagbemiga Awosope <100247352+JAILBREAK-101@users.noreply.github.com>
2024-11-21 14:42:00 +0100

    Update db.js

--- config/my-sql/db.js
+++ config/my-sql/db.js
@@ -1,14 +1,12 @@
 const mysql = require('mysql2');
 
-// Create a connection to the MySQL database
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'your-username',
-  password: 'your-password',
-  database: 'membership_db'
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database
 });
 
-// Connect to the database
 connection.connect((err) => {
   if (err) {
     console.error('Error connecting to the database:', err.stack);
