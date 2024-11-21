1fa5e4390a2130a9064f495aa4120ef202625369
Oluwagbemiga Awosope <100247352+JAILBREAK-101@users.noreply.github.com>
2024-11-21 14:42:42 +0100

    Delete config/firebase/firebaseAdminConfig.js

--- config/firebase/firebaseAdminConfig.js
+++
@@ -1,9 +0,0 @@
-const admin = require("firebase-admin");
-
-// Initialize the Firebase Admin SDK
-admin.initializeApp({
-  credential: admin.credential.cert(require("./path/to/serviceAccountKey.json")),
-  databaseURL: "https://your-project-id.firebaseio.com"
-});
-
-module.exports = admin;
